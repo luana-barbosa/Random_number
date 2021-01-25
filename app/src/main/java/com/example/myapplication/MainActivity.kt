ef30f371f2e7736e45e6dd38a28752e60e3df7f9
Luana Santos Barbosa <ltb@certi.org.br>
2021-01-25 17:15:51 -0300

    [Randomic Numbers]
    * Adicionado numeros a serem sorteados.

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,38 +2,10 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.TextView
-import org.w3c.dom.Text
-import kotlin.random.Random
 
-
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val buttonRandom = findViewById<Button>(R.id.buttonRandom)
-        val textNumber = findViewById<TextView>(R.id.textNumber)
-
-        buttonRandom.setOnClickListener(this)
-        textNumber.setOnClickListener(this)
     }
-
-    override fun onClick(v: View?) {
-        // Código
-        if (v?.id == R.id.buttonRandom || v?.id == R.id.textNumber) {
-            //Lógica
-            val id = random()
-        }
-    }
-
-    fun random(): Int{
-       return Random.nextInt( until = 50) + 1
-
-    }
-
-
 }
-
