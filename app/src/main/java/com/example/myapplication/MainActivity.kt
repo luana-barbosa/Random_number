921d1e7f328cec11e6384fa61ddf474db82ee7e0
Luana Santos Barbosa <ltb@certi.org.br>
2021-01-25 17:59:26 -0300

    Adicionado os eventos de click

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val buttonRandom = findViewById<Button>(R.id.buttonRandom)
+        val textNumber = findViewById<TextView>(R.id.textNumber)
+
+        buttonRandom.setOnClickListener(this)
+        textNumber.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        // Código
+        if (v?.id == R.id.buttonRandom || v?.id == R.id.textNumber) {
+            //Lógica
+        }
     }
 }
